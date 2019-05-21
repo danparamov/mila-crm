7957b9c381d3606dc616ce15f9ff5f46b5de1606
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-05-21 18:03:34 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/logo2.png';
 import LogoMobile from '../assets/logo-mobile.png';
 import Menu from './Menu';
 
@@ -18,21 +18,21 @@ export default class Nav extends Component {
   render() {
     return (
       <div>
-        <nav className="dt w-100 border-box pa3-ns ph5-ns">
-          <Link to="/" title="KIT CRM">
-            <img src={Logo} className="dn dib-m dib-l h3" alt="KIT CRM" />
+        <nav className="w-10-ns">
+          <Link to="/" title="MILA CRM">
+            <img src={Logo} className="" alt="MILA CRM" />
           </Link>
-          <div className="w-100 border-box">
-            <div className="dib fl">
-              <Link to="/" title="KIT CRM">
+          <div className="">
+            <div className="fl-ns">
+              <Link to="/profile" title="MILA CRM">
                 <img
                   src={LogoMobile}
                   className="dn-ns h3 center pl3 align-middle"
-                  alt="KIT CRM"
+                  alt="MILA CRM"
                 />
               </Link>
             </div>
-            <div className="dib right fr" onClick={this.toggleMenu}>
+            <div className="dib left-0 fl-ns" onClick={this.toggleMenu}>
               <img
                 src={this.props.profileImage}
                 className="dn-ns h2 br-100 align-middle pa3"
