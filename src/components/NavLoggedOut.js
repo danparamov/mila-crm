84289635377e36d71e11516dbedf28c57022954f
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-05-21 18:03:36 -0500

    Update NavLoggedOut.js

--- src/components/NavLoggedOut.js
+++ src/components/NavLoggedOut.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/logo2.png';
 
 export default class NavLoggedOut extends Component {
   state = {
@@ -17,8 +17,8 @@ export default class NavLoggedOut extends Component {
     return (
       <div>
         <nav className="dt w-100 border-box pa3-ns ph5-ns">
-          <Link to="/" title="KIT CRM">
-            <img src={Logo} className="dib h3" alt="KIT CRM" />
+          <Link to="/profile" title="MILA CRM">
+            <img src={Logo} className="dib h5" alt="MILA CRM" />
           </Link>
         </nav>
       </div>
