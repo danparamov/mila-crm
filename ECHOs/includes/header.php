7ba6a7ab5faa7989d34fcd0bab6e0c3ce2bb94c5
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-05-21 18:01:43 -0500

    Create header.php

---
+++ ECHOs/includes/header.php
@@ -0,0 +1,37 @@
+<?php include 'includes/sdk.php' ?>
+
+    <header class="header_area">
+        <nav class="navbar navbar-expand-lg menu_one menu_four">
+            <div class="container custom_container p0">
+                <a class="navbar-brand b-img" href="/"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198493/echo/logo/logo_kxpau6.png" alt="logo"></a>
+                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="menu_toggle">
+                        <span class="hamburger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                        <span class="hamburger-cross">
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav menu pl_120">
+                        <li class="nav-item submenu active">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item submenu mega_menu">
+                            <a class="nav-link" href="contact">Contact Us</a>
+                        </li>
+                        <li class="nav-item submenu mega_menu">
+                            <a class="nav-link" href="https://medium.com/@echoisolutions">Blog</a>
+                        </li>
+                    </ul>
+                </div>
+                <a class="btn_get btn_hover hidden-sm hidden-xs" href="contact">Let's Talk</a>
+            </div>
+        </nav>
+    </header>
