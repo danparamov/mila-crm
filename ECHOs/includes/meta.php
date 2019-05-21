3acdc4e97d6b7e53de4fe1b556e370fa17ae6c2e
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-05-21 18:01:45 -0500

    Create meta.php

---
+++ ECHOs/includes/meta.php
@@ -0,0 +1,33 @@
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Echo Intelligent Solutions is a strategic design and technology agency that drives revenue, represent the best companies and create Personal Qualified Leads for your business." />
+  <meta name="robots" content="index, follow" />
+  <meta name="keywords" content="" />
+  <link rel="shortcut icon" href="https://res.cloudinary.com/phlab/image/upload/v1557198500/echo/img/favicon_i6xzhw.png" type="image/x-icon">
+
+  <!-- OpenGraph -->
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="en_us" />
+  <meta property="og:url" content="https://echoisolutions.com" />
+  <meta property="og:title" content="ECHO INTELLIGENT SOLUTIONS" />
+  <meta property="og:description" content="Echo Intelligent Solutions is a strategic design and technology agency that drives revenue, represent the best companies and create Personal Qualified Leads for your business. " />
+  <meta property="og:image" content="https://res.cloudinary.com/phlab/image/upload/v1557198502/echo/img/logo-box_gfnmmo.png" />
+  <meta property="og:image:type" content="image/png" />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+  <!-- Twitter Card -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@echoisolutions" />
+  <meta name="twitter:creator" content="@echoisolutions" />
+  <meta name="twitter:title" content="ECHO INTELLIGENT SOLUTIONS" />
+  <meta name="twitter:description" content="" />
+  <meta name="twitter:image" content="https://res.cloudinary.com/phlab/image/upload/v1557198502/echo/img/logo-box_gfnmmo.png" />
+
+
+  <link rel="manifest" href="../manifest.json" />
+  <meta name="theme-color" content="#7ed957" />
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.min.css">
+  <link rel="stylesheet" href="css/responsive.min.css">
