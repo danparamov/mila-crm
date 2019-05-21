cca64be781c22d728bc8c2bfe4daf1eff5fb27af
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-05-21 18:03:32 -0500

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 export default function Menu(props) {
   return (
     <div className="db bg-black">
-      <Link to="/home" className="db white no-underline b pt3 pb3 tr mr3">
-        Home
+      <Link to="/profile" className="db white no-underline b pt3 pb3 tr mr3">
+        Dashboard
       </Link>
       <Link to="/contacts" className="db white no-underline b pt3 pb3 tr mr3">
         Contacts
       </Link>
       <Link to="/accounts" className="db white no-underline b pt3 pb3 tr mr3">
         Accounts
+      </Link>
+      <Link to="/opportunities" className="db white no-underline b pt3 pb3 tr mr3">
+        Opportunities
       </Link>
       <Link
         to="/settings"
