fb345db4843c12c600b5352fa0f9ca55acaf58b3
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-05-21 18:01:49 -0500

    Create index.php

---
+++ ECHOs/index.php
@@ -0,0 +1,273 @@
+﻿<!doctype html>
+<html lang="en">
+   <head>
+      <title> ECHO INTELLIGENT SOLUTIONS </title>
+      <?php include 'includes/meta.php' ?>
+   </head>
+   <body>
+      <?php include 'includes/header.php' ?>
+      <section class="agency_banner_area bg_color">
+         <img class="banner_shap" src="https://res.cloudinary.com/phlab/image/upload/v1557198497/echo/img/banner_bg_zvm11m.png" alt="">
+         <div class="container custom_container">
+         <div class="row">
+            <div class="col-lg-5 d-flex align-items-center">
+               <div class="agency_content">
+                  <h2 class="f_700 t_color3 mb_40 wow fadeInLeft" data-wow-delay="0.3s">Driving New Technologies Globally</h2>
+                  <div class="action_btn d-flex align-items-center mt_60">
+                     <a href="#start" class="btn_hover agency_banner_btn">Get Started</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-7 text-right">
+               <img class="protype_img wow fadeInRight" data-wow-delay="0.3s" src="https://res.cloudinary.com/phlab/image/upload/v1557198499/echo/img/head-img_nc39ha.svg" alt="">
+            </div>
+         </div>
+      </section>
+      <section id="start" class="agency_about_area d-flex bg_color">
+         <div class="col-lg-6 about_img">
+            <img class="img-fluid" src="https://res.cloudinary.com/phlab/image/upload/v1557198501/echo/img/side_vabzfj.svg" alt="side ill">
+         </div>
+         <div class="col-lg-6 about_content_left ">
+            <div class="about_content mb_30">
+               <h2 class="f_size_30 f_500 l_height45 mb_20">We are in the forefront of new technologies for social good.</h2>
+               <p class="f_size_15 f_300 mb_40">We are a group of entrepreneurs with different backgrounds ranging from finance, sales and engineering.  We have been able to create an extensive business network across the Latin American and Spain business ecosystems -- specializing in driving the adoption of new technologies. Our ecosystems have grown to include  educational institutions, banks, aviation agencies, governments, military,  hospitals, incubators and accelerators.</p>
+            </div>
+         </div>
+      </section>
+      <section class="agency_featured_area bg_color">
+         <div class="container">
+            <h2 class="f_size_30 f_600 t_color3 l_height40 text-center wow fadeInUp" data-wow-delay="0.3s">Our Design Process</h2>
+            <div class="features_info">
+               <img class="dot_img" src="https://res.cloudinary.com/phlab/image/upload/v1557198499/echo/img/dot_b1egwq.png" alt="">
+               <div class="row agency_featured_item flex-row-reverse">
+                  <div class="col-lg-6">
+                     <div class="agency_featured_img text-right wow fadeInRight" data-wow-delay="0.4s">
+                        <img src="https://res.cloudinary.com/phlab/image/upload/v1557198497/echo/img/design1_yc78lq.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="agency_featured_content pr_70 pl_70 wow fadeInLeft" data-wow-delay="0.6s">
+                        <div class="dot"><span class="dot1"></span><span class="dot2"></span></div>
+                        <img class="number" src="https://res.cloudinary.com/phlab/image/upload/v1557198500/echo/img/icon01_devqbf.png" alt="">
+                        <h3>TARGET MARKET AND CUSTOMER MOTIVATION </h3>
+                        <p>An ECHO Specialist will schedule an introductory consultation to understand our client’s product market, their customer market and motivation by identifying the benefits to adopt a specific technology and studying the current and forecasted competition within their market.</p>
+                        <a href="#d2" class="btn_hover agency_banner_btn mt_30">Next ↓</a>
+                     </div>
+                  </div>
+               </div>
+               <div id="d2" class="row agency_featured_item agency_featured_item_two">
+                  <div class="col-lg-6">
+                     <div class="agency_featured_img text-right wow fadeInLeft" data-wow-delay="0.3s">
+                        <img src="https://res.cloudinary.com/phlab/image/upload/v1557198497/echo/img/design2_mia4p6.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="agency_featured_content pl_100 wow fadeInRight" data-wow-delay="0.5s">
+                        <div class="dot"><span class="dot1"></span><span class="dot2"></span></div>
+                        <img class="number" src="https://res.cloudinary.com/phlab/image/upload/v1557198500/echo/img/icon02_txriwn.png" alt="">
+                        <h3>WIN-WIN AGREEMENT</h3>
+                        <p>Most importantly we focus on creating a win-win scenario by concentrating in the best opportunities and strategic decisions.</p>
+                        <a href="#d3" class="btn_hover agency_banner_btn mt_30">Next ↓</a>
+                     </div>
+                  </div>
+               </div>
+               <div id="d3" class="row agency_featured_item flex-row-reverse">
+                  <div class="col-lg-6">
+                     <div class="agency_featured_img text-right wow fadeInRight" data-wow-delay="0.4s">
+                        <img src="https://res.cloudinary.com/phlab/image/upload/v1557198497/echo/img/design3_a8hpdt.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="agency_featured_content pr_70 pl_70 wow fadeInLeft" data-wow-delay="0.6s">
+                        <div class="dot"><span class="dot1"></span><span class="dot2"></span></div>
+                        <img class="number" src="https://res.cloudinary.com/phlab/image/upload/v1557198500/echo/img/icon03_ldw08y.png" alt="">
+                        <h3>LEAD GENERATION</h3>
+                        <p>By getting to know our client’s market and their customer share, we start producing and vetting leads aimed towards generating revenue for our clients.</p>
+                        <a href="#d4   " class="btn_hover agency_banner_btn mt_30">Next ↓</a>
+                     </div>
+                  </div>
+               </div>
+               <div id="d4" class="row agency_featured_item agency_featured_item_two">
+                  <div class="col-lg-6">
+                     <div class="agency_featured_img text-right wow fadeInLeft" data-wow-delay="0.3s">
+                        <img src="https://res.cloudinary.com/phlab/image/upload/v1557198497/echo/img/design4_j0do6d.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="agency_featured_content pl_100 wow fadeInRight" data-wow-delay="0.5s">
+                        <div class="dot"><span class="dot1"></span><span class="dot2"></span></div>
+                        <img class="number" src="https://res.cloudinary.com/phlab/image/upload/v1557198500/echo/img/icon04_picstv.png" alt="">
+                        <h3>QUALIFIED</h3>
+                        <p>We assist with the follow-up calls to establish the decision makers, scope the deal, get an estimated timeline and determine the procurement/acquisition roadmap. </p>
+                        <a href="contact" class="btn_hover agency_banner_btn mt_30">Get in Touch →</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="dot middle_dot"><span class="dot1"></span><span class="dot2"></span></div>
+            </div>
+         </div>
+      </section>
+      <section class="app_service_area app_service_area_two align-center">
+         <div class="container">
+            <div class="sec_title text-center align-center mb_70">
+               <h2 class="f_size_30 f_600 t_color3 l_height40 text-center mb_60">Technologies We Represent</h2>
+            </div>
+            <div class="row app_service_info">
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-money app_icon one"></i>
+                     <h5 class="f_p f_size_18 f_600 t_color3 mt_40 mb-30">Digital Payments</h5>
+                     <p class="f_300 f_size_15 mb-30"></p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-desktop app_icon two"></i>
+                     <h5 class="f_p f_size_18 f_600 t_color3 mt_40 mb-30">Software Development</h5>
+                     <p class="f_300 f_size_15 mb-30"></p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-settings app_icon two"></i>
+                     <h5 class="f_p f_size_18 f_600 t_color3 mt_40 mb-30">Artifical Inteligence and Blockchain Digital Transformation</h5>
+                     <p class="f_300 f_size_15 mb-30"></p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-pulse app_icon three"></i>
+                     <h5 class="f_p f_size_18 f_600 t_color3 mt_40 mb-30">Cancer Test Technology</h5>
+                     <p class="f_300 f_size_15 mb-30"></p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="app_service_area align-center">
+         <div class="container">
+            <div class="sec_title text-center align-center mb_70">
+               <h2 class="f_size_30 f_600 t_color3 l_height40 text-center mb_60">ECHO’s Partnerships</h2>
+            </div>
+         <div class="partner_logo">
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.2s">
+               <a target="_blank" href="https://renaissancepayments.com/" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198495/echo/clients/renapay_oyq52f.png" alt="Renaissance payments logo"></a>
+            </div>
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.4s">
+               <a target="_blank" href="https://learningmachine.com" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198495/echo/clients/learning-machine_hkxsyp.svg" alt="Learning machine logo"></a>
+            </div>
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.5s">
+               <a target="_blank" href="https://philanthrolab.co" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198497/echo/clients/phlab_qx3mhu.svg" alt="Philanthrolab logo"></a>
+            </div>
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.3s">
+               <a target="_blank" href="https://lamoncogroup.com/" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198495/echo/clients/lam_ljlsb1.svg" alt="Lam logo"></a>
+            </div>
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.3s">
+               <a target="_blank" href="https://blockstack.org/" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557199610/echo/clients/blockstack_szzg6x.png" alt="Blockstack logo"></a>
+            </div>
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.3s">
+               <a target="_blank" href="https://xertify.co/" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557199882/echo/clients/xertify_eos39w.png" alt="Xertify logo"></a>
+            </div>
+         </div>
+      </div>
+      </section>
+
+      <section class="app_service_area align-center">
+         <div class="container">
+            <div class="sec_title text-center align-center mb_70">
+               <h2 class="f_size_30 f_600 t_color3 l_height40 text-center mb_20">ECHO’s Strategic Allies</h2>
+            </div>
+         <div class="partner_logo">
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.2s">
+               <a target="_blank" href="https://blockchainacademy.mx/" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198494/echo/allies/bacademy_sp92mz.svg" alt="Blockchain academy logo"></a>
+            </div>
+            <div class="p_logo_item wow fadeInLeft" data-wow-delay="0.4s">
+               <a target="_blank" href="https://blockchainespana.com/" rel="noopener"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198495/echo/allies/bespana_wdnsft.png" alt="Blockchain espana logo"></a>
+            </div>
+         </div>
+       </div>
+      </section>
+      <section class="agency_testimonial_area bg_color sec_pad">
+         <div class="container">
+            <h2 class="f_size_30 f_600 t_color3 l_height40 text-center mb_60">What people are saying..</h2>
+            <div class="agency_testimonial_info">
+               <div class="testimonial_slider owl-carousel">
+                  <div class="testimonial_item text-center left">
+                     <div class="author_img"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198494/echo/test/01_bhlznw.png" alt="Adam davis"></div>
+                     <div class="author_description">
+                        <h4 class="f_500 t_color3 f_size_18">Adam Davis</h4>
+                        <h6>Strategy Manager - LAM</h6>
+                     </div>
+                     <p>Daniel from ECHO Intelligent Solutions, has been a great resource for creating new business.
+                     ECHO has provided high quality work, helping us understand the market and how to access it efficiently.
+                     ECHO facilities and streamlines business development and we look forward to continuing our work together.</p>
+                  </div>
+                  <div class="testimonial_item text-center center">
+                     <div class="author_img"><img src="https://res.cloudinary.com/phlab/image/upload/v1557198494/echo/test/02_wdid88.png" alt="Joseph akintolayo"></div>
+                     <div class="author_description">
+                        <h4 class="f_500 t_color3 f_size_18">Joseph Akintola</h4>
+                        <h6>CEO/Founder - Philanthrolab</h6>
+                     </div>
+                     <p>I have had the pleasure of working with Daniel on many projects.
+                     He has been an excellent resource and diligent in his approach to finding the best solutions to challenging problems.
+                     He really raises the bar with his commincation and overall motivation.
+                     He’s been indispensable and a major part of our client success.
+                     I could not rate him higher</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="app_service_area app_service_area_two">
+         <div class="container">
+            <div class="sec_title text-center mb_70">
+               <h2 class="f_size_30 f_600 t_color3 l_height40 text-center mb_60">Our Sales Funnel</h2>
+            </div>
+            <div class="row app_service_info">
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-dashboard app_icon one"></i>
+                     <h5 class="f_p f_size_16 f_600 t_color3 mt_40 mb-30">LEADS</h5>
+                     <p class="f_300 f_size_20 mb-30">20</p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-bar-chart app_icon two"></i>
+                     <h5 class="f_p f_size_16 f_600 t_color3 mt_40 mb-30">QUALIFIED LEADS</h5>
+                     <p class="f_300 f_size_20 mb-30">4</p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-light-bulb app_icon two"></i>
+                     <h5 class="f_p f_size_16 f_600 t_color3 mt_40 mb-30">CONTRACT </h5>
+                     <p class="f_300 f_size_20 mb-30">2</p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="app_service_item">
+                     <i class="ti-bolt app_icon three"></i>
+                     <h5 class="f_p f_size_16 f_600 t_color3 mt_40 mb-30">REVENUE GENERATED</h5>
+                     <p class="f_300 f_size_20 mb-30">$10M</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="action_area_three sec_pad">
+         <div class="curved"></div>
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="action_content text-center">
+                     <h2 class="f_600 f_size_30 l_height45 mb_40">Ready to increase your reach globally?</h2>
+                     <a href="contact" class="about_btn white_btn wow fadeInLeft" data-wow-delay="0.3s">Let's Talk</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <?php include 'includes/footer.php' ?>
+   </body>
+</html>
