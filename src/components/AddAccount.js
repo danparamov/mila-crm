b69472373cade80ce26893651e52fc619d48e85d
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-05-24 10:12:40 -0500

    Update AddAccount.js

--- src/components/AddAccount.js
+++ src/components/AddAccount.js
@@ -26,11 +26,17 @@ export default class AddAccount extends Component {
     industry: '',
     salesstage: '',
     description: '',
+    accountType: '',
     country: '',
     region: '',
     email: '',
     phoneNumber: '',
+    annualRevenue: '',
+    employees: '',
     twitterHandle: '',
+    telegramId: '',
+    slack: '',
+    addressStreet: '',
     //birthDate: '',
     //sex: '',
     blockstackId: '',
@@ -108,9 +114,15 @@ export default class AddAccount extends Component {
       website: this.state.website,
       industry: this.state.industry,
       salesstage: this.state.salesstage,
+      accountType: this.state.accountType,
+      slack: this.state.accountType,
       description: this.state.description,
+      employees: this.state.employees,
       country: this.state.country,
+      telegramId: this.state.telegramId,
+      addressStreet: this.state.addressStreet,
       region: this.state.region,
+      annualRevenue: this.state.annualRevenue,
       email: this.state.email,
       phoneNumber: this.state.phoneNumber,
       twitterHandle: this.state.twitterHandle,
@@ -137,10 +149,16 @@ export default class AddAccount extends Component {
       //name: '',
       //lastName: '',
       description: '',
+      employees: '',
+      accountType: '',
+      addressStreet: '',
+      slack: '',
+      telegramId: '',
       country: '',
       region: '',
       email: '',
       phoneNumber: '',
+      annualRevenue: '',
       twitterHandle: '',
       //birthDate: '',
       //sex: '',
@@ -193,6 +211,7 @@ export default class AddAccount extends Component {
               }}
             >
               <Error error={error} />
+              <h3 className="">Account Information</h3>
               <fieldset>
                 <label htmlFor="accountname">
                   Account Name
@@ -209,7 +228,7 @@ export default class AddAccount extends Component {
               </fieldset>
               <fieldset>
                 <label htmlFor="website">
-                  Website
+                  Account Website
                   <input
                     type="text"
                     id="website"
@@ -225,21 +244,21 @@ export default class AddAccount extends Component {
                   Industry
                   <select onChange={this.handleChange} id="industry" name="industry">
                     <option value="" defaultChecked>
-                      Select Industry..
+                      Select
                     </option>
                     <option value="Telecomunications">Telecomunications</option>
                     <option value="Education">Education</option>
-                    <option value="Aerospace">Aerospace</option>
-                    <option value="Government">Government</option>
+                    <option value="Aerospace">Large Enterprise</option>
+                    <option value="Government">Government/Military</option>
                   </select>
                 </label>
               </fieldset>
               <fieldset>
                 <label>
-                  Sales Stage
+                  Rating
                   <select onChange={this.handleChange} id="salesstage" name="salesstage">
                     <option value="" defaultChecked>
-                      Select Stage..
+                      Select
                     </option>
                     <option value="Acquired">Acquired</option>
                     <option value="Active">Active</option>
@@ -249,41 +268,54 @@ export default class AddAccount extends Component {
                 </label>
               </fieldset>
               <fieldset>
-                <label htmlFor="description">
-                  Description
+                <label>
+                  Account Type
+                  <select onChange={this.handleChange} id="accountType" name="accountType">
+                    <option value="" defaultChecked>
+                      Select
+                    </option>
+                    <option value="Analyst">Analyst</option>
+                    <option value="Competitor">Competitor</option>
+                    <option value="Customer">Customer</option>
+                    <option value="Distributor">Distributor</option>
+                    <option value="Integrator">Integrator</option>
+                    <option value="Investor">Investor</option>
+                    <option value="Partner">Partner</option>
+                    <option value="Press">Press</option>
+                    <option value="Prospects">Prospects</option>
+                    <option value="Reseller">Reseller</option>
+                    <option value="Supplier">Supplier</option>
+                    <option value="Vendor">Vendor</option>
+                  </select>
+                </label>
+              </fieldset>
+              <fieldset>
+                <label htmlFor="employees">
+                  Employees
                   <input
-                    type="description"
-                    id="description"
-                    name="description"
-                    placeholder="Description.."
-                    value={this.state.description}
+                    type="text"
+                    id="employees"
+                    name="employees"
+                    placeholder=""
+                    value={this.state.employees}
                     onChange={this.handleChange}
                   />
                 </label>
               </fieldset>
               <fieldset>
-                <label htmlFor="country">
-                  Country
-                  <CountryDropdown
-                    name="country"
-                    id="country"
-                    value={this.state.country}
-                    onChange={val => this.selectCountry(val)}
+                <label htmlFor="annualRevenue">
+                  Annual Revenue
+                  <input
+                    type="text"
+                    id="annualRevenue"
+                    name="annualRevenue"
+                    placeholder="$"
+                    value={this.state.annualRevenue}
+                    onChange={this.handleChange}
                   />
                 </label>
               </fieldset>
-              <fieldset>
-                <label htmlFor="region">
-                  Region
-                  <RegionDropdown
-                    name="region"
-                    id="region"
-                    country={this.state.country}
-                    value={this.state.region}
-                    onChange={val => this.selectRegion(val)}
-                  />
-                </label>
-              </fieldset>
+              <h3 className="">Social</h3>
               <fieldset>
                 <label htmlFor="email">
                   Email
@@ -317,7 +349,7 @@ export default class AddAccount extends Component {
                     type="text"
                     id="twitterHandle"
                     name="twitterHandle"
-                    placeholder="Twitter handle.. (eg. danparamov)"
+                    placeholder=""
                     value={this.state.twitterHandle}
                     onChange={this.handleChange}
                   />
@@ -325,7 +357,7 @@ export default class AddAccount extends Component {
               </fieldset>
               <fieldset disabled={loading} aria-busy={loading}>
                 <label htmlFor="blockstackId">
-                  Blockstack Id
+                  Blockstack ID
                   <input
                     type="text"
                     id="blockstackId"
@@ -333,6 +365,69 @@ export default class AddAccount extends Component {
                     placeholder="Blockstack ID.."
                     value={this.state.blockstackId}
                     onChange={this.handleChange}
+                  />
+                </label>
+              </fieldset>
+              <fieldset disabled={loading} aria-busy={loading}>
+                <label htmlFor="telegramId">
+                  Telegram ID
+                  <input
+                    type="text"
+                    id="telegramId"
+                    name="telegramId"
+                    placeholder=""
+                    value={this.state.blockstackId}
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </fieldset>
+              <fieldset disabled={loading} aria-busy={loading}>
+                <label htmlFor="Slack">
+                  Slack
+                  <input
+                    type="text"
+                    id="slack"
+                    name="slack"
+                    placeholder=""
+                    value={this.state.slack}
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </fieldset>
+              <h3 className="">Address Information</h3>
+              <fieldset disabled={loading} aria-busy={loading}>
+                <label htmlFor="addressStreet">
+                  Address Street
+                  <input
+                    type="text"
+                    id="addressStreet"
+                    name="addressStreet"
+                    placeholder=""
+                    value={this.state.addressStreet}
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </fieldset>
+              <fieldset>
+                <label htmlFor="country">
+                  Country
+                  <CountryDropdown
+                    name="country"
+                    id="country"
+                    value={this.state.country}
+                    onChange={val => this.selectCountry(val)}
+                  />
+                </label>
+              </fieldset>
+              <fieldset>
+                <label htmlFor="region">
+                  Region
+                  <RegionDropdown
+                    name="region"
+                    id="region"
+                    country={this.state.country}
+                    value={this.state.region}
+                    onChange={val => this.selectRegion(val)}
                   />
                 </label>
               </fieldset>
@@ -351,6 +446,20 @@ export default class AddAccount extends Component {
                   <option value="C">Every three months</option>
                   <option value="D">Every year</option>
                   </select>
+                </label>
+              </fieldset>
+              <h3 className="">Description Information</h3>
+              <fieldset>
+                <label htmlFor="description">
+                  Description
+                  <input
+                    type="description"
+                    id="description"
+                    name="description"
+                    placeholder=""
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                  />
                 </label>
               </fieldset>
               <button type="submit" className="bg-black">
