4c08a5124cec04829b47b1201027cca798e0abd8
jparamo07 <50684116+jparamo07@users.noreply.github.com>
2019-05-27 17:49:51 -0500

    Update SingleContact.js

--- src/components/SingleContact.js
+++ src/components/SingleContact.js
@@ -12,7 +12,7 @@ export default class SingleContact extends Component {
           <table className="Table f3">
            <tbody>
             <tr>
-              <td className="Cell"><Link
+              <td className="Cell" style={{width:1000}}><Link
                 to={{
                 pathname: '/contact',
                 search: `?id=${contact.id}`,
@@ -20,11 +20,11 @@ export default class SingleContact extends Component {
               className="f4 link dim ph3 pv2 mb2 dib white bg-green b--black br-100 w3"> >>>
               </Link>
               </td>
-              <td className="Cell" style={{width:100}}>{contact.name}</td>
-              <td className="Cell" style={{width:100}}>{contact.email}</td>
-              <td className="Cell" style={{width:100}}>{contact.phoneNumber}</td> 
-              <td className="Cell" style={{width:100}}>{contact.country}</td>
-              <td className="Cell" style={{width:100}}><PriorityLabel priority={contact.priority} small/></td>
+              <td className="Cell" style={{width:2000}}>{contact.name}</td>
+              <td className="Cell" style={{width:2000}}>{contact.email}</td>
+              <td className="Cell" style={{width:1000}}>{contact.phoneNumber}</td> 
+              <td className="Cell" style={{width:1000}}>{contact.country}</td>
+              <td className="Cell" style={{width:1000}}><PriorityLabel priority={contact.priority} small/></td>
             </tr>
            </tbody>
           </table>
