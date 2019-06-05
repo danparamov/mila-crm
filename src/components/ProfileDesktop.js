672fb3e7ab41c37d77594a750d7ebb24b1f40258
danparamov <daniel.paramo.v@gmail.com>
2019-06-05 16:22:01 -0500

    Update ProfileDesktop.js

--- src/components/ProfileDesktop.js
+++ src/components/ProfileDesktop.js
@@ -7,91 +7,292 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Logo from '../assets/milalogo.jpg';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import SearchIcon from '@material-ui/icons/Search';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Contacts from '@material-ui/icons/Face';
+import Dashboard from '@material-ui/icons/Dashboard';
+import Accounts from '@material-ui/icons/Group';
+import Tasks from '@material-ui/icons/Assignment';
+import Settings from '@material-ui/icons/Settings';
+import Opportunities from '@material-ui/icons/Favorite';
+import Updates from '@material-ui/icons/Update';
+import Logout from '@material-ui/icons/Power';
+import { withStyles } from '@material-ui/core/styles';
 
-function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  );
-}
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})(props => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
 
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+const StyledMenuItem = withStyles(theme => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: 'auto',
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
   },
 }));
+
 
 function ProfileDesktop(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
 
   function handleChange(event, newValue) {
     setValue(newValue);
   }
 
   return (
-      <AppBar>
-        <Tabs variant="fullWidth" value={value} onChange={handleChange}>
-            <Link
-              to="/profile"
-              className="f3 link dim ph3 pv1 mb2 dib white w-10"
-            >
-              <Tab label="Dashboard" />
-            </Link>
-            <Link
-              to="/contacts"
-              className="f3 link dim ph3 pv1 mb2 dib white w-10"
-            >
-              <Tab label="Contacts" />
-            </Link>
-            <Link
-              to="/accounts"
-              className="f3 link dim ph3 pv1 mb2 dib white w-10"
-            >
-              <Tab label="Accounts" />
-            </Link>
-            <Link
-              to="/opportunities"
-              className="f3 link dim ph3 pv1 mb2 dib white w-10"
-            >
-              <Tab label="Opportunities" />
-            </Link>
-            <Link
-              to="/tasks"
-              className="f3 link dim ph3 pv1 mb2 dib white w-10"
-            >
-              <Tab label="Tasks" />
-            </Link>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+        <Button
+          aria-controls="customized-menu"
+          aria-haspopup="true"
+          variant="contained"
+          color="primary"
+          onClick={handleClick}
+        >
+          Menu
+        </Button>
+        <StyledMenu
+          id="customized-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <StyledMenuItem>
+            <ListItemIcon>
             <Link
               to="/settings"
-              className="f3 link dim ph3 pv1 mb2 dib white w-10"
-            >
-              <Tab label="Settings" />
+              className="link dim dib green"
+              >
+            <div>
+              <img
+                src={props.profileImage}
+                className="h3 w3 br-100"
+                id="avatar-image"
+                alt=""
+              />
+            </div>
+            <p className="h6">
+              <span id="heading-name">{props.name}</span>
+              <Typography className="green">{props.username}</Typography>
+            </p>
             </Link>
+            </ListItemIcon>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Dashboard />
+            </ListItemIcon>
+              <Link
+                to="/profile"
+                className="link dim dib green"
+              >
+                <Typography>
+                 Dashboard
+                </Typography>
+              </Link>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Contacts />
+            </ListItemIcon>
+            <Link
+              to="/contacts"
+              className="link dim dib green"
+            >
+              <Typography>
+               Contacts
+              </Typography>
+            </Link>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Accounts />
+            </ListItemIcon>
+            <Link
+              to="/accounts"
+              className="link dim dib green"
+            >
+              <Typography>
+               Accounts
+              </Typography>
+            </Link>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Opportunities />
+            </ListItemIcon>
+            <Link
+              to="/opportunities"
+              className="link dim dib green"
+            >
+              <Typography>
+               Opportunities
+              </Typography>
+            </Link>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Tasks />
+            </ListItemIcon>
+            <Link
+              to="/tasks"
+              className="link dim dib green"
+            >
+              <Typography>
+               Tasks
+              </Typography>
+            </Link>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <Link
+              to="/settings"
+              className="link dim dib green"
+            >
+              <Typography>
+               Settings
+              </Typography>
+            </Link>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Updates />
+            </ListItemIcon>
             <Link
               to="/updates"
-              className="f3 link dim ph3 pv1 mb2 dib white w-10"
+              className="link dim dib green"
             >
-              <Tab label="Updates" />
+              <Typography>
+               Updates
+              </Typography>
             </Link>
-              <a
-                className="pointer f3 link dim ph3 pv1 mb2 dib white w-10"
-                id="signout-button"
-                onClick={props.logout}
-              >
-                <Tab label="Logout" />
-              </a>
-            <Link to="/" title="MILA CRM">
-              <img src={Logo} className="w-50" alt="MILA CRM" align="right"/>
-            </Link><br /><br />
-            </Tabs>
-        </AppBar>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <ListItemIcon>
+              <Logout />
+            </ListItemIcon>
+            <a
+              className="link dim dib green"
+              id="signout-button"
+              onClick={props.logout}
+            >
+            <Typography>
+             Logout
+            </Typography>
+            </a>
+          </StyledMenuItem>
+        </StyledMenu>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon/>
+            </div>
+            <InputBase
+              placeholder="Search..."
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'Search'}}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
