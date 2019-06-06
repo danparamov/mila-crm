153ebcb62da7a6c6b660cdc5110735bb99b27a2d
danparamov <daniel.paramo.v@gmail.com>
2019-06-06 15:54:35 -0500

    Create archive.js

---
+++ src/components/archive.js
@@ -0,0 +1,19 @@
+
+
+<div className="mb4 pt4">
+  <img src={MilaLogo} alt="Mila Logo" />
+  <h1 className="f1 mb0">Grow Your Sales Funnel With Mila</h1>
+  <h2 className="mt0 f4">Keeping your data where it belongs</h2>
+</div>
+<p>
+  <DefaultButton
+    className="f6 ph3 pv2 mb2 dib white bg-green b--black pointer"
+    id="signin-button"
+    onClick={handleSignIn.bind(this)}
+    primary
+  >
+    Start Now
+  </DefaultButton>
+</p>
+<LearnMore />
+<GlobalStyle />
