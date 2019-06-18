831e8c5fd9e25215f7ca76b8e00d7a3cb60b7e7e
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-06-18 13:42:57 -0500

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -13,6 +13,18 @@ import SingleContactTasks from './SingleContactTasks';
 import SingleAccountTasks from './SingleAccountTasks';
 import SingleOppTasks from './SingleOppTasks';
 import ifAttribute from './util/ifAttribute';
+import AddOppIcon from '@material-ui/icons/noteadd';
+import AddContactIcon from '@material-ui/icons/personadd';
+import AddAccountIcon from '@material-ui/icons/addtophotos';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper'
+import PriorityLabel from './PriorityLabel';
+import'./Styles/Table.css';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default class Profile extends Component {
   state = {
@@ -67,13 +79,23 @@ export default class Profile extends Component {
     const { contacttasks } = this.state;
     const { accounttasks } = this.state;
     const { opptasks } = this.state;
+    const classes = makeStyles(theme => ({
+      root: {
+        width: '100%',
+        marginTop: theme.spacing(3),
+        overflowX: 'auto',
+      },
+      table: {
+        minWidth: 650,
+      },
+    }));
     let ContactBlock = null;
     let AccountBlock = null;
     let OppBlock = null;
- 
+
     if (ifAttribute(contacttasks[0])) {
       ContactBlock = (
-        <div className="w-100 w-75-ns fl ph4 tl">
+        <div className="w-100 w-200-ns fl ph4 tl">
              {contacttasks.map(contacttask => (
              <SingleContactTasks contacttask={contacttask} key={contacttask.id} />
             ))}
@@ -85,7 +107,7 @@ export default class Profile extends Component {
 
     if (ifAttribute(accounttasks[0])) {
         AccountBlock = (
-          <div className="w-100 w-75-ns fl ph4 tl">
+          <div className="w-100 w-200-ns fl ph4 tl">
             {accounttasks.map(accounttask => (
               <SingleAccountTasks accounttask={accounttask} key={accounttask.id} />
             ))}
@@ -97,7 +119,7 @@ export default class Profile extends Component {
 
     if (ifAttribute(opptasks[0])) {
       OppBlock = (
-        <div className="w-100 w-75-ns fl ph4 tl">
+        <div className="w-100 w-200-ns fl ph4 tl">
           {opptasks.map(opptask => (
             <SingleOppTasks opptask={opptask} key={opptask.id} />
           ))}
@@ -106,7 +128,7 @@ export default class Profile extends Component {
     } else {
         OppBlock = null;
     }
- 
+
     return !isSignInPending() ? (
       <div>
         <Nav
@@ -117,41 +139,50 @@ export default class Profile extends Component {
         />
         <div className="mw9 center ph3 cf">
           <div className="w-100 w-75-ns f4 ph4 tl">
-            <h1> Contacts
+            <h1>Tasks </h1>
             <Link
               to={{
-                pathname: '/contacttasks'
+                pathname: '/add-contacttask'
               }}
               className="f2 link dim ph3 pv2 mb2 dib white bg-green b--black"
-            > > 
-            </Link></h1>
-          </div>
-          {ContactBlock} 
-          
-          <div className="w-100 w-75-ns f4 ph4 tl">
-            <h1> Accounts
+            >
+            <AddContactIcon />
+            </Link>
             <Link
               to={{
-                pathname: '/accounttasks',
+                pathname: '/add-accounttask'
               }}
-              className="f2 link dim ph3 pv2 mb2 dib white bg-yellow b--black"
-            > >
-            </Link></h1> 
-          </div>
-          {AccountBlock}
-          <br /> <br /> 
-          <div className="w-100 w-75-ns f4 ph4 tl">
-            <h1> Opportunities
+              className="f2 link dim ph3 pv2 mb2 dib white bg-red b--black"
+            >
+            <AddAccountIcon />
+            </Link>
             <Link
               to={{
-                pathname: '/opptasks',
+                pathname: '/add-opptask'
               }}
               className="f2 link dim ph3 pv2 mb2 dib white bg-blue b--black"
-            > >  
-            </Link></h1> 
+            >
+            <AddOppIcon />
+            </Link>
+            <Paper className={classes.root}>
+              <Table className={classes.table}>
+                <TableHead></TableHead>
+                  <TableBody>
+                      <TableRow>
+                      <TableCell width="20%" align="left">Name</TableCell>
+                      <TableCell width="20%" align="left">Subject</TableCell>
+                      <TableCell width="20%" align="left">Due Date</TableCell>
+                      <TableCell width="20%" align="left">Priority</TableCell>
+                      <TableCell width="20%" align="left">Status</TableCell>
+                      </TableRow>
+                  </TableBody>
+              </Table>
+            </Paper>
           </div>
+          {ContactBlock}
+          {AccountBlock}
           {OppBlock}
-        </div>
+        </div>  
       </div>
     ) : null;
   }
