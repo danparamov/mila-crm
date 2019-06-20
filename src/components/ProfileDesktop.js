b6bb5267df0b13955dede1cf77f1b6f075f8a6cb
Daniel Paramo <daniel.paramo.v@gmail.com>
2019-06-20 14:45:03 -0500

    Update ProfileDesktop.js

--- src/components/ProfileDesktop.js
+++ src/components/ProfileDesktop.js
@@ -6,7 +6,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Logo from '../assets/milalogo.jpg';
+import Logo from '../assets/logo_transparent.png';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
